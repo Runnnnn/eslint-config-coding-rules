7857897747f45c3ee22dae47808327a74b41dd69
kaite huang <huangkaite@youzan.com>
2021-05-25 16:41:27 +0800

    feat: init

--- index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const espree = require('espree');
-const config = require('./lib/crconfig');
-
-// eslint-disable-next-line no-console
-console.log(13123);
-
-const ast = espree.parse('code', {});
